e359cc5e3994200faba781f9c0075918b09350d9
Francesca P <francescapuche@gmail.com>
2019-05-30 20:23:28 -0400

    adding question

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -11,3 +11,67 @@ $(document).on("click", "#start", function(e) {
     game.loadQuestion(); 
 }); 
 
+var quizQuestions = [
+    {
+        qNum: 1,
+        question: "Curacao is a small country in the Carribean",
+        answers: ["True", "False"],
+        correctAnswer: "True"
+    }, 
+    {
+        qNum: 2,
+        question: "Montana is the least populated state in the US.",
+        answers: ["True", "False"],
+        correctAnswer: "",
+    }, 
+    {
+        qNum: 3,
+        question: " ",
+        answers: ["True", "False"],
+        correctAnswer: ""
+    }, 
+    {
+        qNum: 4,
+        question: " ",
+        answers: ["True", "False"],
+        correctAnswer: "",
+    },
+    {
+        qNum: 5,
+        question: " ",
+        answers: ["True", "False"],
+        correctAnswer: ""
+    }, 
+    {
+        qNum: 6,
+        question: " ",
+        answers: ["True", "False"],
+        correctAnswer: "",
+    },
+    {
+        qNum: 7,
+        question: " ",
+        answers: ["True", "False"],
+        correctAnswer: ""
+    }, 
+    {
+        qNum: 8,
+        question: " ",
+        answers: ["True", "False"],
+        correctAnswer: " ",
+    },
+    {
+        qNum: 9,
+        question: " ",
+        answers: ["True", "False"],
+        correctAnswer: "",
+    },
+    {
+        qNum: 10,
+        question: " ",
+        answers: ["True", "False"],
+        correctAnswer: "",
+    }
+  ];
+
+  
